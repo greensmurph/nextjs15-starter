3f921df024288f538cb005b7b973d0699ff9e9bc
Jonathan Self <jon.self@live.com>
2025-04-16 21:51:42 -0500

    Switch to server build timestamp

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-export default function Page() {
-  const buildTime = new Date().toISOString();
+export async function getStaticProps() {
+  return {
+    props: {
+      timestamp: new Date().toISOString(),
+    }
+  }
+}
 
+type Props = {
+  timestamp: string;
+};
+
+export default function Page({ timestamp }: Props) {
   return (
     <main>
       <h1>Test page</h1>
+      <p>Cached at the edge with Cloudflare!</p>
       <footer style={{ marginTop: '2rem', fontSize: '0.9em', color: '#888' }}>
-        Page rendered at: {buildTime}
+        Page rendered at: {timestamp}
       </footer>
     </main>
   );
